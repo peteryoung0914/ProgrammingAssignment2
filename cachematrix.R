a0186e21208d93966b89e05053db0ef2d60d02b6
peteryoung0914 <51043363+peteryoung0914@users.noreply.github.com>
2019-07-12 11:56:40 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a script made for an R Programming Assignment on lexical scoping
 
-## Write a short comment describing this function
+## These functions cache the inverse of a matrix (makeCacheMatrix) and retrieve
+## a cached inverse matrix if available (cacheSolve)
+
+## Example input and output: 
+## > matrix1 <- makeCacheMatrix(matrix(c(4, 0, 0, 4, 1, 1, 0, 1, 0), c(3, 3)))
+## > cacheSolve(matrix1)
+## Already in cache! Retrieving cached data
+##      [,1] [,2] [,3]
+## [1,] 0.25    0   -1
+## [2,] 0.00    0    1
+## [3,] 0.00    1   -1
+
+## FUNCTION 1: makeCacheMatrix
+## Set and get the value of a matrix, set and get the value of an inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 i <- NULL
@@ -19,7 +31,9 @@ i <- NULL
 }
 
 
-## Write a short comment describing this function
+
+## FUNCTION 2: cacheSolve
+## Returns the inverse of a matrix created with makeCacheMatrix, retrieving from cache if present
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
